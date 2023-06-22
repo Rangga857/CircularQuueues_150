bed8aae7ec45c82b4d845ade7bacaae155999636
Rangga857 <azharrangga28@gmail.com>
2023-06-22 15:53:18 +0700

    debugging final

--- circularqueues/circularqueues.cpp
+++ circularqueues/circularqueues.cpp
@@ -97,8 +97,8 @@ int main() {
 			cout << "Menu" << endl;
 			cout << "1. Implement insert operation" << endl;
 			cout << "2. Implement delete operation" << endl;
-			cout << "Display values" << endl;
-			cout << "exit" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. exit" << endl;
 			cout << "Enter your choice (1-4): ";
 			cin >> ch;
 			cout << endl;
@@ -117,7 +117,7 @@ int main() {
 				break;
 			}
 			case'4': {
-				return;
+				return 0;
 			}
 			default: {
 				cout << "Invalid option!" << endl;
